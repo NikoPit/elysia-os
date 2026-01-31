2b745aa9b22ce1dd0b977bfedd622e96995e904a
elysia <3302843873@qq.com>
2026-01-31 19:37:52 +0800

    Added stackoverflow test

---
+++ tests/stack_overflow.rs
@@ -0,0 +1,52 @@
+#![no_std]
+// Disables main function to customize entry point
+#![no_main]
+#![feature(custom_test_frameworks, abi_x86_interrupt)]
+// renames main function for testing because we disabled main with #[no_main]
+#![reexport_test_harness_main = "test_main"]
+#![test_runner(testing::run_tests)]
+use elysia_os::debug_exit::debug_exit;
+use elysia_os::os::get_os;
+use elysia_os::print;
+use elysia_os::s_print;
+use elysia_os::s_println;
+use elysia_os::testing;
+use volatile::VolatilePtr;
+use volatile::VolatileRef;
+use x86_64::instructions::interrupts::int3;
+// Disable dynamic linking with the std library because there is no std library in our own os
+
+use core::panic::PanicInfo;
+
+use elysia_os::panic_handler;
+use elysia_os::panic_handler::test_handle_panic;
+use elysia_os::println;
+use elysia_os::test;
+
+// Disables name mangling so the linker can recognize the entry point
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    s_print!("\nStack overflow double-fault handling ");
+    get_os().init();
+
+    stack_overflow();
+
+    s_println!("[FAILED]\n");
+    s_println!("Test continued to run after stack overflow\n");
+    debug_exit(elysia_os::debug_exit::QemuExitCode::Failed);
+
+    loop {}
+}
+
+#[allow(unconditional_recursion)]
+fn stack_overflow() {
+    stack_overflow();
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    s_println!("[OK]\n");
+    s_println!("Test success!");
+    debug_exit(elysia_os::debug_exit::QemuExitCode::Success);
+    loop {}
+}
