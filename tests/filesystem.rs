cb85f69be54d4499ba5ee00d9186fb2b524fd3d1
elysia <3302843873@qq.com>
2026-02-09 15:51:44 +0800

    unit tests for filesystem

--- tests/filesystem.rs
+++ tests/filesystem.rs
@@ -17,12 +17,73 @@ entry_point!(k_main);
 fn k_main(bootinfo: &'static BootInfo) -> ! {
     init(bootinfo);
 
-    s_println!("todo");
-    debug_exit(elysia_os::debug_exit::QemuExitCode::Success);
+    test_main();
+
+    s_println!("\nSomething went wrong. Process contiuned after test");
+    debug_exit(elysia_os::debug_exit::QemuExitCode::Failed);
 
     hlt_loop();
 }
 
+extern crate alloc;
+
+use alloc::string::ToString;
+
+use elysia_os::{
+    filesystem::{
+        path::Path,
+        vfs::{FileData, VirtualFS},
+    },
+    test,
+};
+
+test!("VFS Basic", || {
+    let a_txt = Path::new("/test/vfs_create.txt");
+    VirtualFS.lock().create_file(a_txt.clone()).unwrap();
+    VirtualFS
+        .lock()
+        .write_file(
+            a_txt.clone(),
+            FileData {
+                content: "abc".to_string(),
+            },
+        )
+        .unwrap();
+    let content = VirtualFS.lock().read_file(a_txt.clone()).unwrap().content;
+
+    assert_eq!(content, "abc");
+});
+
+test!("VFS Create Dir", || {
+    let a_txt = Path::new("/test/vfs_dir");
+    VirtualFS.lock().create_dir(a_txt.clone()).unwrap();
+});
+
+test!("VFS List Contents", || {
+    let a = Path::new("/tests/dir/a.txt");
+    let b = Path::new("/tests/dir/b.txt");
+    let c = Path::new("/tests/dir/c.txt");
+    VirtualFS.lock().create_file(a.clone()).unwrap();
+    VirtualFS.lock().create_file(b.clone()).unwrap();
+    VirtualFS.lock().create_file(c.clone()).unwrap();
+    VirtualFS.lock().write_file(
+        a,
+        FileData {
+            content: "gwergwegf".to_string(),
+        },
+    );
+
+    let content = VirtualFS
+        .lock()
+        .list_contents(Path::new("/tests/dir"))
+        .unwrap();
+
+    assert!({
+        content.contains(&"a.txt".to_string())
+            && content.contains(&"b.txt".to_string())
+            && content.contains(&"c.txt".to_string())
+    });
+});
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     test_handle_panic(info)
