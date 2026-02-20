85f93cac15ae5820978ceca235631b1de8cc317e
elysia <3302843873@qq.com>
2026-02-20 16:21:30 +0800

    uses naked functions on syscall entry

--- kernel/src/systemcall/entry.rs
+++ kernel/src/systemcall/entry.rs
@@ -2,31 +2,30 @@ use crate::print;
 
 // entry point for all system calls
 #[unsafe(no_mangle)]
+#[unsafe(naked)]
 pub extern "C" fn syscall_entry() {
-    unsafe {
-        core::arch::asm!(
-            // Pushing arguments required for SyscallSnapshot
-            "push rcx",
-            "push r11",
-            "push rax",
-            "push rdi",
-            "push rsi",
-            "push rdx",
-            // rsp: stack pointer
-            // rdi: 1st argument
-            // calls syscall_handler with the things
-            // we've pushed to the stack
-            "mov rdi, rsp",
-            "call syscall_handler",
-            // resume
-            "pop rdx",
-            "pop rsi",
-            "pop rdi",
-            "pop rax", // rust have modified it to be the return value
-            "pop r11",
-            "pop rcx",
-            // resume the state
-            "sysret"
-        )
-    };
+    core::arch::naked_asm!(
+        // Pushing arguments required for SyscallSnapshot
+        "push rcx",
+        "push r11",
+        "push rax",
+        "push rdi",
+        "push rsi",
+        "push rdx",
+        // rsp: stack pointer
+        // rdi: 1st argument
+        // calls syscall_handler with the things
+        // we've pushed to the stack
+        "mov rdi, rsp",
+        "call syscall_handler",
+        // resume
+        "pop rdx",
+        "pop rsi",
+        "pop rdi",
+        "pop rax", // rust have modified it to be the return value
+        "pop r11",
+        "pop rcx",
+        // resume the state
+        "sysret"
+    )
 }
