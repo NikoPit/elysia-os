16cf2652832d540d067021cef0db207ef34e8270
elysia <3302843873@qq.com>
2026-02-20 12:15:39 +0800

    Moved test-init into the workspace

---
+++ test-init/src/main.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![no_main]
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    trigger_syscall();
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+fn trigger_syscall() {
+    let syscall_number = 1; // write
+    let fd = 1;
+    let msg = b"helloworldz";
+    let buf = msg.as_ptr();
+    let count = msg.len();
+
+    unsafe {
+        core::arch::asm!(
+            "syscall",
+            in("rax") syscall_number,
+            in("rdi") fd,
+            in("rsi") buf,
+            in("rdx") count,
+            out("rcx") _, // 系统调用会破坏rcx和r11
+            out("r11") _,
+        );
+    }
+}
