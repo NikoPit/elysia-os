fa819d05eedffd9ecaeb16d31ac92557604892f5
elysia <3302843873@qq.com>
2026-02-22 13:31:10 +0800

    refactored the build script

--- libc-test/build.sh
+++ libc-test/build.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
-as crt0.s -o crt0.o
-clang --target=x86_64-unknown-none -ffreestanding -fno-stack-protector -c main.c -o main.o
-ld.lld -nostdlib -static crt0.o main.o ../relibc/target/x86_64-unknown-linux-gnu/release/librelibc.a -o test.elf
+# 1. 配置路径 (根据你的实际存放位置修改)
+RELIBC_PATH="../relibc/target/x86_64-unknown-linux-gnu/release"
+RELIBC_INCLUDE="../relibc/include"
+RELIBC_LIB="$RELIBC_PATH"
+
+# 2. 编译参数
+# --target: 指定目标为 x86_64-unknown-redox
+# -ffreestanding: 不使用宿主机的标准库
+TARGET="x86_64-unknown-none"
+
+# 3. 编译源文件为对象文件
+clang --target=$TARGET \
+      -ffreestanding \
+      -fno-stack-protector \
+      -I$RELIBC_INCLUDE \
+      -c main.c -o main.o
+
+# 4. 链接阶段 (核心步骤)
+# -static: 静态链接
+# -nostdlib: 禁用所有默认库，我们要手动指定
+# 顺序极其重要: crt0.o 必须在最前面
+ld.lld -nostdlib -static \
+       ./crt0.o \
+       main.o \
+       $RELIBC_LIB/librelibc.a \
+       -o test.elf
