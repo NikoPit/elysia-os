99af27af8c63ba77c3651f4b8844df28356caa9d
elysia <3302843873@qq.com>
2026-02-22 18:36:23 +0800

    Fixed some bugs on auxv

--- kernel/src/utils/aux.rs
+++ kernel/src/utils/aux.rs
@@ -1,7 +1,7 @@
 pub enum AuxType {
     Null = 0,
     ProgramHeaderTable = 3,
-    ProgramHeaderNT = 4,
+    ProgramHeaderEntrySize = 4,
     ProgramHeaderAmount = 5,
     PageSize = 6,
     EntryPointAddress = 9,
--- kernel/src/utils/stack_builder.rs
+++ kernel/src/utils/stack_builder.rs
@@ -1,10 +1,12 @@
 use elfloader::ElfBinary;
 use x86_64::VirtAddr;
+use xmas_elf::program;
 
 use crate::utils::aux::AuxType;
 
 #[derive(Debug)]
 pub struct StackBuilder {
+    origin: u64,
     sp: VirtAddr,
     write_sp: *mut u64,
 }
@@ -12,20 +14,32 @@ pub struct StackBuilder {
 impl StackBuilder {
     pub fn new(sp: u64, write_sp: *mut u64) -> Self {
         Self {
+            origin: sp,
             sp: VirtAddr::new(sp),
             write_sp,
         }
     }
 
     pub fn push_aux_entries(&mut self, file: &ElfBinary) {
+        let mut base = 0;
+
+        for ele in file.program_headers() {
+            if matches!(ele.get_type().unwrap(), program::Type::Load) && base == 0 {
+                base = ele.virtual_addr();
+            }
+        }
+
         self.push_aux_entry(AuxType::Null, 0);
         self.push_aux_entry(AuxType::EntryPointAddress, file.entry_point());
         self.push_aux_entry(
             AuxType::ProgramHeaderAmount,
             file.program_headers().count() as u64,
         );
-        self.push_aux_entry(AuxType::ProgramHeaderNT, 5);
-        self.push_aux_entry(AuxType::ProgramHeaderTable, 0);
+        self.push_aux_entry(AuxType::ProgramHeaderEntrySize, 56);
+        self.push_aux_entry(
+            AuxType::ProgramHeaderTable,
+            base + file.file.header.pt2.ph_offset(),
+        );
         self.push_aux_entry(AuxType::PageSize, 4096);
     }
 
