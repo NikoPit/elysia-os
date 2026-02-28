ee10bb7a677d1dce91c10779f9d666d8236d548a
elysia <3302843873@qq.com>
2026-02-28 14:02:25 +0800

    Added object configurtation stuffs

--- kernel/src/object.rs
+++ kernel/src/object.rs
@@ -27,3 +27,12 @@ pub trait Readable: Object {
     /// Reads the content of [`self`] and write them to [`buffer`]
     fn read(&self, buffer: &mut [u8]) -> ObjectResult<usize>;
 }
+
+pub enum ConfigurateRequest {
+    GetWindowSize,
+    GetTerminalInfo,
+}
+
+pub trait Configuratable: Object {
+    fn configure(&self, request: ConfigurateRequest) -> ObjectResult<isize>;
+}
