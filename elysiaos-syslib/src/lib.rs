e5d342393dc390309374f68543818839c78ae3b3
elysia <3302843873@qq.com>
2026-03-01 13:33:11 +0800

    Changed elysiaos-syslib to static lib to be compiled to .a file

--- elysiaos-syslib/src/lib.rs
+++ elysiaos-syslib/src/lib.rs
@@ -1,7 +1,17 @@
 #![no_std]
 
+use core::panic::PanicInfo;
+
+use crate::syscalls::{object::write_object, print};
+
 pub mod c_wrapper;
 pub mod errors;
 pub mod numbers;
 pub mod syscalls;
 pub mod utils;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    print("elyisaos-syslib panic").unwrap();
+    loop {}
+}
