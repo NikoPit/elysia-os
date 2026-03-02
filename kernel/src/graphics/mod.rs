c2d5e69f41ebb51cdb0aa71a2db124353c4ac80e
elysia <3302843873@qq.com>
2026-03-02 21:03:43 +0800

    Moved graphics.rs to graphics/mod.rs

--- kernel/src/graphics/mod.rs
+++ kernel/src/graphics/mod.rs
@@ -11,7 +11,7 @@ pub mod object;
 pub mod object_config;
 pub mod tty;
 
-pub static FONT: &[u8] = include_bytes!("../../maplemono.psf");
+pub static FONT: &[u8] = include_bytes!("../../../maplemono.psf");
 
 pub fn init(boot_info: &'static mut bootloader_api::info::FrameBuffer) {
     FRAME_BUFFER.get_or_init(|| Mutex::new(Canvas::new(boot_info)));
