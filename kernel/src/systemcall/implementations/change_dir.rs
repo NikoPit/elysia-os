57170dbbbd46a45bb6b078221848e59ea65a07e7
elysia <3302843873@qq.com>
2026-03-04 16:02:05 +0800

    actually implemented change_dir syscall

--- kernel/src/systemcall/implementations/change_dir.rs
+++ kernel/src/systemcall/implementations/change_dir.rs
@@ -1,4 +1,12 @@
-use crate::systemcall::{implementations::utils::SyscallImpl, syscall_no::SyscallNo};
+use core::{ptr::from_raw_parts, str::from_utf8};
+
+use alloc::slice;
+
+use crate::{
+    filesystem::path::Path,
+    multitasking::MANAGER,
+    systemcall::{implementations::utils::SyscallImpl, syscall_no::SyscallNo},
+};
 
 pub struct ChangeDirImpl;
 
@@ -13,6 +21,11 @@ impl SyscallImpl for ChangeDirImpl {
         arg5: u64,
         arg6: u64,
     ) -> Result<usize, crate::systemcall::error::SyscallError> {
-        unimplemented!()
+        let target = unsafe { slice::from_raw_parts(arg1 as *const u8, arg2 as usize) };
+        let process = MANAGER.lock().current.clone().unwrap();
+
+        process.lock().current_directory = Path::new(from_utf8(target).unwrap());
+
+        Ok(0)
     }
 }
