e2d7c39a7bcd5766f088c72081fbdb05d319cab9
elysia <3302843873@qq.com>
2026-03-07 16:31:58 +0800

    implemented execve (maybe?)

---
+++ kernel/src/multitasking/process/execve.rs
@@ -0,0 +1,51 @@
+use alloc::{str::pattern::StrSearcher, string::String, vec::Vec};
+
+use crate::{
+    filesystem::{path::Path, vfs::VirtualFS},
+    multitasking::{
+        process::{
+            Process,
+            misc::{init_objects, init_stack_layout},
+        },
+        thread::{
+            THREAD_MANAGER,
+            snapshot::{ThreadSnapshot, ThreadSnapshotType},
+            thread::Thread,
+        },
+    },
+    userspace::elf_loader::load_elf,
+};
+
+impl Process {
+    pub fn execve(&mut self, path: Path, args: Vec<String>) {
+        let thread = THREAD_MANAGER
+            .get()
+            .unwrap()
+            .lock()
+            .current
+            .clone()
+            .unwrap();
+
+        let mut program =
+            alloc::vec![0u8; VirtualFS.lock().file_info(path.clone()).unwrap().size as usize];
+        VirtualFS.lock().read_file(path.clone(), &mut program);
+
+        let kernel_stack_top = self.addrspace.allocate_kernel(16).1.finish();
+        let mut stack_builder = self.addrspace.allocate_user(16).1;
+        let program = load_elf(&mut self.addrspace, &mut program);
+        self.kernel_stack_top = kernel_stack_top;
+
+        assert!(!program.is_pie(), "Pie program is not supported for now");
+
+        init_stack_layout(&mut stack_builder, &program);
+
+        thread.lock().snapshot = ThreadSnapshot::new(
+            program.entry_point() as u64,
+            &mut self.addrspace,
+            stack_builder.finish().as_u64(),
+            ThreadSnapshotType::Thread,
+        );
+
+        init_objects(&mut self.objects);
+    }
+}
